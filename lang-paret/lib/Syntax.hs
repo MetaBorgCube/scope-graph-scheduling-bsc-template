c5e66866d78e873f8cebf85e8a494f78fa5a3f41
Arraying <paul.huebner@googlemail.com>
2023-04-21 15:54:06 +0200

    Create rough framework.

--- lang-paret/lib/Syntax.hs
+++ lang-paret/lib/Syntax.hs
@@ -1,17 +1,27 @@
 module Syntax where
+import Data.List (intercalate)
 
 data Type
   = NumT
+  | BoolT Bool
   | FunT Type Type
+  | ListT Type
+  | TupT [Type]
   deriving Eq
--- To use inference, replace `Type` with
--- type Ty = Term Int
--- (Term imported from Data.Term)
--- See also `lang-hm/Syntax` for an example.
 
 data Expr
   = Num Int
+  | Tru
+  | Fls
   | Plus Expr Expr
+  | Conditional Expr Expr Expr
+  | Nil Type -- No inferrence yet.
+  | Cons Expr Expr
+  | Head Expr
+  | Tail Expr
+  | Tuple [Expr]
+  | Index Int Expr
+  | Let (String, Type, Expr) Expr
   | App Expr Expr
   | Ident String
   | Abs String Type Expr
@@ -19,7 +29,10 @@ data Expr
 
 instance Show Type where
   show NumT = "num"
+  show (BoolT v) = show v
   show (FunT ti to) = "(" ++ show ti ++ " -> " ++ show to ++ ")"
+  show (ListT i) = "[" ++ show i ++ "]"
+  show (TupT t) = "(" ++ intercalate ", " (map show t) ++ ")"
 
 example :: Expr
 example = App (Abs "x" NumT (Plus (Ident "x") (Ident "x"))) (Num 21)
